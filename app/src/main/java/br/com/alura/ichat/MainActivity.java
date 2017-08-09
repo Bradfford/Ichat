3c3f4cc9c099b0b4fa8cc1e39060fc1204e5dd32
Millfford <bradfford@hotmail.com>
2017-08-09 10:52:16 -0300

    Inclusão de serviço de integração com o servidor de mensgens

--- app/src/main/java/br/com/alura/ichat/MainActivity.java
+++ app/src/main/java/br/com/alura/ichat/MainActivity.java
@@ -2,30 +2,67 @@ package br.com.alura.ichat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import br.com.alura.ichat.adapter.MensagemAdapter;
 import br.com.alura.ichat.modelo.Mensagem;
+import br.com.alura.ichat.service.ChatService;
 
 public class MainActivity extends AppCompatActivity {
 
     private int idCliente = 1;
+    private EditText editText;
+    private Button button;
+    private ListView listaMensagens;
+    private List<Mensagem> mensagens;
+    private ChatService chatService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listaMensagens = (ListView) findViewById(R.id.lv_mensagens);
+        listaMensagens = (ListView) findViewById(R.id.lv_mensagens);
 
-        List<Mensagem> mensagens = Arrays.asList(new Mensagem(1, "Olá alunos!"), new Mensagem(2,"oi"));
+        //mensagens = Arrays.asList(new Mensagem(1, "Olá alunos!"), new Mensagem(2,"oi"));
+        mensagens = new ArrayList<>();
 
         MensagemAdapter adapter = new MensagemAdapter(idCliente, mensagens, this);
 
         listaMensagens.setAdapter(adapter);
 
+        chatService = new ChatService(this);
+
+        chatService.ouvirMensagem();
+
+        button = (Button) findViewById(R.id.btn_enviar);
+
+        editText = (EditText) findViewById(R.id.texto_enviar);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ChatService(MainActivity.this).enviar(new Mensagem(idCliente, editText.getText().toString()));
+            }
+        });
+
+    }
+
+    public void exibirMensagem(Mensagem mensagem){
+        mensagens.add(mensagem);
+
+        MensagemAdapter adapter = new MensagemAdapter(idCliente, mensagens, this);
+
+        listaMensagens.setAdapter(adapter);
+
+        chatService.ouvirMensagem();
     }
 }
